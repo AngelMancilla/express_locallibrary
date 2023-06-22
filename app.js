0e5e80e6647ebb1452c93d1c52ca65dd148bbe8b
AngelMancilla <angelmancilla0114@gmail.com>
2023-06-22 10:17:19 -0700

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var app = express();
 // Set up rate limiter: maximum of twenty requests per minute
 const RateLimit = require("express-rate-limit");
 const limiter = RateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 20,
+  windowMs: 15 * 60 * 1000, // 1 minute
+  max: 100,
 });
 
 // Set up mongoose connection
